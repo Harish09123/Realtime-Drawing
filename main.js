a94222f40f578604c97f5c2f90d94d1f5042875f
Harish09123 <108905133+Harish09123@users.noreply.github.com>
2023-07-07 10:33:22 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWrist = 0;
+leftWrist = 0;
+
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(560, 100);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function draw()
+{
+    background('#969A97');
+
+    document.getElementById("square_side").innerHTML = "Width And Heigth of a Square will be = " + difference +"px";
+    fill('#F90093');
+    stroke('#F90093');
+    square(noseX, noseY, difference);
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet has been inisalized");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX=" +noseX+ "noseY=" +noseY);
+
+        leftWrist = results[0].pose.leftWrist.x;
+        rightWrist = results[0].pose.rightWrist.x;
+        difference = floor(leftWrist - rightWrist);
+
+        console.log("leftWrist = " + leftWrist + "rightWrist = " + rightWrist + "difference = " + difference);
+    }
+}
